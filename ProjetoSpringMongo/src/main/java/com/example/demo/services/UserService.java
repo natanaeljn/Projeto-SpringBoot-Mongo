c6fe7f46c50a4727ba6b16d22a6140cd40476d89
natanaeljn <natanaeljn@outlook.com.br>
2022-08-18 21:46:50 -0300

    Inserçao de usuario pelo Post

--- ProjetoSpringMongo/src/main/java/com/example/demo/services/UserService.java
+++ ProjetoSpringMongo/src/main/java/com/example/demo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.services.exception.objectNotFoundException;
 
@@ -27,5 +28,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new objectNotFoundException("Objeto não encontrado"));
 		}
+	
+	/*metodo para inserir*/
+	public User inser(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
